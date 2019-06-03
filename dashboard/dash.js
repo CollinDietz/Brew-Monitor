137c0ba6d5562092c22c5650bd24045c6e298826
Collin Dietz <c4dietz@gmail.com>
2019-06-02 21:25:15 -0400

    Made dashboard to view temp

---
+++ dashboard/dash.js
@@ -0,0 +1,14 @@
+function GetTemp() {
+  fetch("https://api.particle.io/v1/devices/4c0032000751353530373132/temp?access_token=7dbbca9e394ce880d1c2dc5dd97913dd677c5c1e") // Call the fetch function passing the url of the API as a parameter
+    .then(function(response) {
+      response.json().then(function(data) {
+        document.getElementById("temp").innerHTML = data["result"].toFixed(3) + "  " + String.fromCharCode(176) + "F";
+        document.getElementById("timestamp").innerHTML = "Last updated: " + new Date().toLocaleString();
+      });
+    })
+    .catch(function() {});
+
+    setTimeout(arguments.callee, 1000);
+}
+
+GetTemp();
