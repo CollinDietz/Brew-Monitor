3e5f379708892104d4c660db70d9fa169448347d
Collin Dietz <c4dietz@gmail.com>
2020-02-26 00:39:56 -0500

    Added 12 hour history data

--- src/BrewMonitor.ino
+++ src/BrewMonitor.ino
@@ -8,20 +8,46 @@
 #include "DS18.h"
 
 double temp;
+int historyRecords;
 DS18 tempSensor(D6);
+const int hoursToTrack = 12;
+char history[hoursToTrack];
 const system_tick_t pollPeriod = 250;
+const system_tick_t pollPeriodForHistory = 3600000;
+system_tick_t timeOfLastRead = 0;
+system_tick_t timeOfLastReadForHistory = 0;
 
 void setup()
 {
   Particle.variable("temp", temp);
+  Particle.variable("history", history);
+  Particle.variable("historyRecords", historyRecords);
+
+  for(int i = 0; i < hoursToTrack; i++)
+  {
+    history[i] = 0;
+  }
+
+  historyRecords = 0;
 }
 
 void loop()
 {
-  system_tick_t timeOfLastRead = 0;
   if (millis() - timeOfLastRead >= pollPeriod && tempSensor.read())
   {
     temp = tempSensor.fahrenheit();
     timeOfLastRead = millis();
+  }
+
+  if(millis() - timeOfLastReadForHistory >= pollPeriodForHistory)
+  {
+    for(int i = hoursToTrack - 1; i > 0; i--)
+    {
+      history[i] = history[i-1];
+    }
+
+    history[0] = (int)(temp);
+    timeOfLastReadForHistory = millis();
+    historyRecords++;
   }
 }
