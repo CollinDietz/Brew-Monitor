6f4c0b69186f4e1877f9fe31d0f1b2a19c67b08c
Collin Dietz <c4dietz@gmail.com>
2020-02-25 23:38:32 -0500

    Make code non blocking, remove compiled cpp file

--- src/BrewMonitor.ino
+++ src/BrewMonitor.ino
@@ -9,15 +9,19 @@
 
 double temp;
 DS18 tempSensor(D6);
+const system_tick_t pollPeriod = 250;
 
-void setup() {
+void setup()
+{
   Particle.variable("temp", temp);
 }
 
-void loop() {
-  if (tempSensor.read()) {
-   temp = tempSensor.fahrenheit();
-   delay(250);
- }
-
+void loop()
+{
+  system_tick_t timeOfLastRead = 0;
+  if (millis() - timeOfLastRead >= pollPeriod && tempSensor.read())
+  {
+    temp = tempSensor.fahrenheit();
+    timeOfLastRead = millis();
+  }
 }
